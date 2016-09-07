17c74394343afed83ddbc332d4fcdb79a7094653
snobu <foo@snobu.org>
2016-09-07 11:43:13 +0300

    Added unique codename for gifs and permalinks

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//
+var Moniker = require('moniker');
 var wget = require('wget-improved');
 var util = require('util');
 var formidable = require('formidable');
@@ -7,44 +7,68 @@ var http = require('http');
 var gm = require('gm');
 var magick = gm.subClass({imageMagick: true});
 
-function fetch_gif(gifurl, callback) {
-    var fetch_dest = '/tmp/in.gif'
+function get_random_name() {
+    var name = Moniker.generator([Moniker.adjective, Moniker.noun]);
+    return name.choose();
+}
+
+function fetch_gif(gifurl, infile, callback) {
     var options = {};
-    var download = wget.download(gifurl, fetch_dest, options);
+    var download = wget.download(gifurl, infile, options);
     download.on('error', function(err) {
         console.log(err);
     });
-    download.on('start', function(fileSize) {
-        console.log('Download started - ' + fileSize);
+    download.on('start', function(filesize) {
+        console.log('Fetching gif to: ' + infile);
+        console.log('Download started: ' + filesize);
     });
     download.on('end', function(output) {
         console.log(output);
         callback();
     });
     download.on('progress', function(progress) {
-        if (progress == 1) console.log('wget completed: ' + progress)
+        if (progress == 1) {
+            console.log('wget finished: ' + progress);
+        }
     });
 }
 
 
 function do_magick(request, response) {
+    var name = get_random_name();
+    var infile = '/tmp/' + name;
+    console.log('infile set to: ' + infile);
+    var outfile = 'p/' + name + '.gif';
+    console.log('outfile set to: ' + outfile);
+    // Make sure output directory exists
+    var outdir = 'p';
+    if (!fs.existsSync(outdir)) {
+        try {
+            fs.mkdirSync(outdir);
+        }
+        catch (e) {
+            console.error('Unable to create output directory: ' + e);
+        }
+        
+    }
     var form = new formidable.IncomingForm();
     form.parse(request, function (err, fields, files) {
         var gifurl = fields.gifurl;
         var pictext = fields.text;
         console.log('Got text from form: ' + pictext);
-        fetch_gif(gifurl, function () {
-            console.log('Calling imagemagick for ' + pictext)
-	    magick('/tmp/in.gif')
+        fetch_gif(gifurl, infile, function () {
+            console.log('Calling imagemagick for ' + pictext);
+	    magick(infile)
 	      .stroke("#000000")
 	      .fill('#ffffff')
 	      .font("./impact.ttf", 42)
 		.drawText(0, 0, pictext, 'South')
 	      .monitor()
-	      .write("./out.gif", function (err) {
+	      .write(outfile, function (err) {
 	          if (!err) {
 	              console.log('Image processing done.');
-		      show_gif(response);
+                      console.log('outfile: ' + outfile);
+		      redirect_to_outfile(response, name);
 		  }
 		  else console.log(err);
               });
@@ -65,15 +89,24 @@ function displayForm(response) {
 }
 
 
-function show_gif(response) {
-        var img = fs.readFileSync('out.gif');
-        response.writeHead(200, {'Content-Type': 'image/gif'});
-        response.end(img, 'binary');
+function redirect_to_outfile(response, name) {
+        response.writeHead(302, {
+            'Location': '/p/' + name + '.gif'
+        });
+        response.end();
 }
 
 
 function onRequest(request, response) {
-    if (request.method == 'GET') {
+    if (request.method == 'GET' && request.url.match(/^\/p\/.+/)) {
+        console.log('request.url = ' + request.url);
+        var img = fs.readFileSync(request.url.replace('/p/', 'p/'));
+        response.writeHead(200, {
+            'Content-Type': 'image/gif'
+        });
+        response.end(img, 'binary');
+    }
+    else if (request.method == 'GET') {
         displayForm(response);
     }
     else if (request.method == 'HEAD') {
@@ -87,4 +120,4 @@ function onRequest(request, response) {
 }
  
 http.createServer(onRequest).listen(process.env.PORT || 3000);
-console.log('Listening for requests.');
+console.log('Listening for requests on port ' + (process.env.PORT || 3000));
